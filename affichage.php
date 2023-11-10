d54f83fcbe9f8237ee4b05ee2e9e341a3c0e4fe2
_N3m0 <n3m0o7@hotmail.com>
2023-11-10 11:17:52 +0100

    envoie nom user dans affichage

---
+++ affichage.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "affichage de l'utilisater : ".$_POST["nom_user"];
+
+?>
--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
 echo "ouistiti";
+?>
+
+<form action="affichage.php" method="post">
+Utilisateur : <input type="text" name="nom_user"><br>
+<input type="submit" value="afficher">
+</form>
