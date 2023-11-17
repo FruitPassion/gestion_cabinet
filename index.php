2ad5264011c07a955f4d65e92fe160d064b4f77c
FruitPassion <gamehotdl@gmail.com>
2023-11-17 10:39:57 +0100

    Ajout partials (nav, head, footer)

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
-<?php
-echo "ouistiti";
-?>
+<!DOCTYPE html>
+<html lang="fr" data-bs-theme="dark">
+
+<?php include 'partial/head.php'; ?>
+
+<body>
+
+<?php include 'partial/nav.php'; ?>
 
 <form action="affichage.php" method="post">
-Utilisateur : <input type="text" name="nom_user"><br>
-<input type="submit" value="afficher">
+    <p>Utilisateur :</p>
+    <input type="text" name="nom_user"><br>
+    <input type="submit" value="afficher">
 </form>
+
+<?php include 'partial/footer.php'; ?>
+
+</html>
