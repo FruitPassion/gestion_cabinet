3f9f80b4343632d1e46616d5be423be190c39441
_N3m0 <n3m0o7@hotmail.com>
2023-11-17 12:10:34 +0100

    redirection patient inexistant

--- affichage.php
+++ affichage.php
@@ -1,5 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr" data-bs-theme="dark">
 
+<?php include 'partial/head.php'; ?>
+
+<body>
+
+<?php include 'partial/nav.php'; ?>
+
+<?php
 $abs = $_SERVER['DOCUMENT_ROOT'];
 include_once($abs."/gestion_cabinet/oop_data/EtatCivil.class.php");
 include_once($abs."/gestion_cabinet/oop_data/Medecin.class.php");
@@ -15,18 +23,26 @@ function afficherMed(){
   echo "affichage du medecin : ".$_POST["nom_med"]."<br>";
 }
 
-function afficherPat(){
+function afficherPat($u){
+  if ($_POST['nom_user'] != $u->getEtatCivil()->getNom()){
+    echo 'utilisateur inexistant<br><a href="/">retour au menu</a>';
+    return;
+  }
+
   echo "affichage de l'utilisateur : ".$_POST["nom_user"]."<br>";
 
-  $medref = $usr->getMedecinReferrant->getEtatCivil->getNom();
+  //$medref = $u->getMedecinReferrant->getEtatCivil->getNom();
+  $medref = "hatoz";
 
   echo '<form action="affichage.php" method="post">';
-  echo '<input type="submit" value="afficher son medecin" name="'.$medref.'">';
+  echo '<p>Afficher son medecin :</p>';
+  echo '<input type="text" readonly name="nom_med" value="'.$medref.'">';
+  echo '<input type="submit" value="afficher">';
   echo '</form>';
 }
 
 if (isset($_POST['nom_user'])){
-  afficherPat();
+  afficherPat($usr);
 } else if (isset($_POST['nom_med'])){
   afficherMed();
 }else{
@@ -34,4 +50,9 @@ if (isset($_POST['nom_user'])){
   exit(1);
 }
 
+include_once("./partial/footer.php")
 ?>
+
+<?php include 'partial/footer.php'; ?>
+
+</html>
